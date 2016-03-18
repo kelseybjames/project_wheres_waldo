8a9202a80be19530aec25235f06a154fe66fc7cd
Thomas Lo <thomasjinlo@gmail.com>
2016-03-18 16:57:32 -0700

    noooo

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -4,32 +4,43 @@ var mouseTracker = (function($){
     $('.photo-container').on('click', function(event){
       var x = event.pageX - 25;
       var y = event.pageY - 25;
+      var $list = $('.names-list').detach();
+      $('.notset').remove();
       
-      var $tag = $('<div class="tag"></tag>');
+      var $tag = $('<div class="tag notset"></tag>');
+
+      console.log($tag);
+
       $tag.css({top: y, left: x})
       $('.photo-container').append($tag);
-      $tag.append($('.names-list'));
-      $('.names-list').show();
+      $tag.append($list);
+      $list.show();
     })
-  }
+  };
 
-  var createTag = function() {
-    $('.photo-container').mousemove(function(event) {
-      var x = event.pageX - 25;
-      var y = event.pageY - 25;
+  var clickName = function() {
+    $('.list-content li').on('click', function(event) {
 
-      var $tag = $('.tag hidden');
-      $tag.css({top: y, left: x});
-    })
+      event.stopPropagation();
+      var $newTag = $('<tag class="tag"></tag>')
+      var $tag = $('.notset')
+      console.log($tag.position())
+      $newTag.css($tag.position());
+
+      var $name = $('<div class="tag-name">' + $(this).html() + '</div>');
+      $newTag.append($name);
+      $('.photo-container').append($newTag);
+    } )
   }
 
   return {
     showTag: showTag,
-    createTag: createTag
+    clickName: clickName
   }
 
 })($);
 
 $(document).ready(function(){
   mouseTracker.showTag();
+  mouseTracker.clickName();
 });
