5ae94495f105a978f175947ef8493d38c586b7b0
Kelsey James <kelseybjames@gmail.com>
2016-03-18 16:08:10 -0700

    tagging waldo

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -8,11 +8,24 @@ var mouseTracker = (function($){
       var $tag = $('<div class="tag"></tag>');
       $tag.css({top: y, left: x})
       $('.photo-container').append($tag);
+      $tag.append($('.names-list'));
+      $('.names-list').show();
+    })
+  }
+
+  var createTag = function() {
+    $('.photo-container').mousemove(function(event) {
+      var x = event.pageX - 25;
+      var y = event.pageY - 25;
+
+      var $tag = $('.tag hidden');
+      $tag.css({top: y, left: x});
     })
   }
 
   return {
-    showTag: showTag
+    showTag: showTag,
+    createTag: createTag
   }
 
 })($);
